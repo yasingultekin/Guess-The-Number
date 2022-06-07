9c8cee354e243f6a189566f43551f0ef6896ae8e
yasingultekin <yasingultekin13@gmail.com>
2022-06-07 18:43:18 +0300

    changes added.

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ let heart = document.querySelector('.heart');
 let restart = document.querySelector('.restart');
 let check = document.querySelector('.check');
 let message = document.querySelector('.message');
+let gameEnd = document.querySelector('.game-end');
+const lets = document.querySelector('.lets');
+const please = document.querySelector('.please');
 const inputContainer = document.querySelector('.input-container');
 const numberInput = document.querySelector('#number');
-let life = 2;
+let life = 5;
 let random = Math.floor(Math.random() * 101);
 
 console.log(random);
@@ -14,24 +17,41 @@ console.log(random);
 
 const guessNumber = function () {
     if (life > 1) {
-        if (random == numberInput.value) {
-            message.innerText = `Congrats!`;
+        if (numberInput.value > 0 && numberInput.value < 100) {
+            if (random == numberInput.value) {
+                message.innerText = `Congrats!`;
+                numberInput.value = '';
+            }
+            else if (numberInput.value < random) {
+                message.innerText = `This is too lower!\nPlease enter a high number.`;
+                start.innerText = numberInput.value;
+                numberInput.value = '';
+                life--;
+                heart.innerText = life;
+            }
+            else if (numberInput.value > random) {
+                message.innerText = `This is too high!\nPlease enter a lower number.`;
+                end.innerText = numberInput.value;
+                numberInput.value = '';
+                life--;
+                heart.innerText = life;
+            }
+            
+        }
+        else {
+            message.innerText = `Please enter a valid number!`
             numberInput.value = '';
         }
-        else if (numberInput.value < random) {
-            message.innerText = `This is too high!\nPlease enter a lower number.`;
-            start.innerText = numberInput.value;
-            numberInput.value = '';
-            life--;
-            he
-        }
-        else if (numberInput.value > random) {
-            message.innerText = `This is too lower!\nPlease enter a high number.`;
-            end.innerText = numberInput.value;
-            numberInput.value = '';
-            life--;
-        }
+        
     }
+     else if (life <= 1) {
+        numberInput.value = '';
+        numberInput.style.display = 'none';
+        check.style.display = 'none';
+        message.style.display = 'none';
+        restart.style.display = 'block';
+        gameEnd.innerText = `Sorry! GAME OVER!`;
+     }
 }
 
 
