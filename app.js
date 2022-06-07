41e1b2de912228868d134fda63f59cc6ff41e8cb
yasingultekin <yasingultekin13@gmail.com>
2022-06-07 12:05:59 +0300

    changes added.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 let start = document.querySelector('.start');
 let end = document.querySelector('.end');
 let heart = document.querySelector('.heart');
+let restart = document.querySelector('.restart');
+let check = document.querySelector('.check');
+const inputContainer = document.querySelector('.input-container');
 const numberInput = document.querySelector('#number');
+let life = 2;
+let random = Math.floor(Math.random() * 101);
 
 
+
+
+    inputContainer.addEventListener('click', (e)=> {
+
+        if (e.target.classList.contains('check')) {
+            
+        }
+    
+    
+
+    })
+
+ 
