f0ab24dc1477e534e566a19f98e868401875a093
yasingultekin <yasingultekin13@gmail.com>
2022-06-07 10:17:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let start = document.querySelector('.start');
+let end = document.querySelector('.end');
+let heart = document.querySelector('.heart');
+const numberInput = document.querySelector('#number');
+
+
