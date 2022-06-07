ad4ae1a5d1d7708f39e0b82c72cfcc948a7333a2
yasingultekin <yasingultekin13@gmail.com>
2022-06-07 17:16:38 +0300

    changes added.

--- app.js
+++ app.js
@@ -3,22 +3,39 @@ let end = document.querySelector('.end');
 let heart = document.querySelector('.heart');
 let restart = document.querySelector('.restart');
 let check = document.querySelector('.check');
+let message = document.querySelector('.message');
 const inputContainer = document.querySelector('.input-container');
 const numberInput = document.querySelector('#number');
 let life = 2;
 let random = Math.floor(Math.random() * 101);
 
+console.log(random);
 
 
-
-    inputContainer.addEventListener('click', (e)=> {
-
-        if (e.target.classList.contains('check')) {
-            
+const guessNumber = function () {
+    if (life > 1) {
+        if (random == numberInput.value) {
+            message.innerText = `Congrats!`;
+            numberInput.value = '';
         }
-    
-    
+        else if (numberInput.value < random) {
+            message.innerText = `This is too high!\nPlease enter a lower number.`;
+            start.innerText = numberInput.value;
+            numberInput.value = '';
+            life--;
+        }
+        else if (numberInput.value > random) {
+            message.innerText = `This is too lower!\nPlease enter a high number.`;
+            end.innerText = numberInput.value;
+            numberInput.value = '';
+            life--;
+        }
+    }
+}
 
-    })
 
- 
+window.onload = () => {
+    numberInput.focus();
+};
+
+check.addEventListener('click', guessNumber);
